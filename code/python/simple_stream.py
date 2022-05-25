5e50fcb5e55f46bea74310be3fb6a0eaafe33cb7
Mathias <mathias.lechelon@univ-amu.fr>
2022-05-25 14:43:10 +0200

    py modif

--- code/python/simple_stream.py
+++ code/python/simple_stream.py
@@ -32,7 +32,10 @@ THE SOFTWARE.
 
 import serial
 import time
+import subprocess
 
+#start camera
+subprocess.Popen("raspistill -t 0", shell=True)
 # Open grbl serial port
 s = serial.Serial("/dev/ttyACM0", 115200)
 
@@ -40,23 +43,25 @@ s = serial.Serial("/dev/ttyACM0", 115200)
 f = open("../gcode/simple_gcode.gcode", "r")
 
 # Wake up grbl
-s.write("\r\n\r\n")
+s.write(b"\r\n\r\n")
 time.sleep(2)  # Wait for grbl to initialize
 s.flushInput()  # Flush startup text in serial input
 
 # Stream g-code to grbl
 for line in f:
     l = line.strip()  # Strip all EOL characters for consistency
+    l = bytes(l, "utf-8")
     print(
-        "Sending: " + l,
+        b"Sending: " + l,
     )
-    s.write(l + "\n")  # Send g-code block to grbl
+    s.write(l + b"\n")  # Send g-code block to grbl
     grbl_out = s.readline()  # Wait for grbl response with carriage return
-    print(" : " + grbl_out.strip())
+    print(b" : " + grbl_out.strip())
 
 # Wait here until grbl is finished to close serial port and file.
-input("  Press <Enter> to exit and disable grbl.")
+# input("  Press <Enter> to exit and disable grbl.")
 
 # Close file and serial port
 f.close()
 s.close()
+subprocess.Popen("pkill raspistill", shell=True)
